339cf86dfe5c6308c66b00f7038a96cc18b70b44
Joey Jeantet <jeantetjoey.pro@gmail.com>
2024-04-20 15:41:38 +0200

    updated server for affliction system

--- stream_wave.go
+++ stream_wave.go
@@ -10,6 +10,7 @@ import (
 )
 
 var spawns = []string{}
+var afflictions = []string{}
 
 func addMonster(w http.ResponseWriter, r *http.Request) {
 	var monster = r.URL.Path[len("/add/"):]
@@ -19,13 +20,27 @@ func addMonster(w http.ResponseWriter, r *http.Request) {
 func fetchMonsters(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Fetched queue next wave soon\n")
 	io.WriteString(w, strings.Join(spawns[:], "|"))
-	// empty the queue
 	spawns = []string{}
+}
+
+func addAffliction(w http.ResponseWriter, r *http.Request) {
+	var affliction = r.URL.Path[len("/add_affliction/"):]
+	afflictions = append(afflictions, affliction)
+	fmt.Printf("Added %s to queue\n", affliction)
+}
+
+func fetchAffliction(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Fetched afflictions next afflictions wave soon\n")
+	io.WriteString(w, strings.Join(afflictions[:], "|"))
+	afflictions = []string{}
 }
 
 func main() {
 	http.HandleFunc("/add/", addMonster)
 	http.HandleFunc("/fetch", fetchMonsters)
+
+	http.HandleFunc("/add_affliction/", addAffliction)
+	http.HandleFunc("/fetch_affliction", fetchAffliction)
 
 	fmt.Printf("Starting server on port 51525\n")
 	err := http.ListenAndServe(":51525", nil)
